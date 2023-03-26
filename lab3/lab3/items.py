911b7496de832841dd86e8aa3b7a984e96543faa
Cumparty <leonidnechay@gmail.com>
2023-03-26 13:28:12 +0300

    lab3

---
+++ lab3/lab3/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class InstituteItem(scrapy.Item):
+    inst_name = scrapy.Field()
+    url = scrapy.Field()
+    img_url = scrapy.Field()
+class DepartmentItem(scrapy.Item):
+    dep_name = scrapy.Field()
+    url = scrapy.Field()
+    institute = scrapy.Field()
